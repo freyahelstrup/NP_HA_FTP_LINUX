a25b61294301b108d48cdc86105b7d8e73a03539
Freya Helstrup <freyagh@me.com>
2017-05-02 15:37:19 +0200

    Edited main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ main.cpp
 using namespace std;
 
 int main() {
-    //char ip[] = "130.226.195.126";
     char ip[] = "192.168.43.11";
     char *ip_pointer = ip; //Casting ip-address to a char-pointer
     FTPClient c, data;
@@ -25,68 +24,57 @@ int main() {
     c.RecvMsg();
     c.SendMsg("PASS s165232@dtu.dk\r\n", 21);
     c.RecvMsg();
-/*
+
     //Entering Passive Mode
     c.SendMsg("PASV\r\n", 6);
     sscanf(c.RecvMsg(), "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d).\r\n", &a1, &a2, &a3, &a4, &p1, &p2);
     dataPort = (p1 * 256) + p2;
 
-    //Opening new data connection to attempt to STOR file in server root dir.
+    //Opening data connection to retrieve log.csv
     data.Connect(dataPort, ip_pointer);
-    c.SendMsg("LIST\r\n", 6);
-    c.RecvMsg();
-
-    data.RecvMsg();
-    c.RecvMsg();
-
-    c.SendMsg("STOR CMakeLists.txt\r\n", 21);
-    c.RecvMsg();
-    data.CloseCon();
-
-
-    //Entering Passive Mode again
-    c.SendMsg("PASV\r\n", 6);
-    sscanf(c.RecvMsg(), "227 Entering Passive Mode (%d,%d,%d,%d,%d,%d).\r\n", &a1, &a2, &a3, &a4, &p1, &p2);
-    dataPort = (p1 * 256) + p2;
-*/
-    //Opening data connection to RETR 1st file
-    data.Connect(dataPort, ip_pointer);
-    c.SendMsg("RETR log.csv\r\n", 14); //small file of 12 bytes
+    c.SendMsg("RETR log.csv\r\n", 14);
     c.RecvMsg();
     c.RecvMsg();
     data.SaveFile("log.csv");
     data.CloseCon();
 
-    //log.csv download
+    //Open log.csv
     ifstream log;
-    string line;
-    int sensor4max;
     log.open("../log.csv", ios::in);
 
+    //Find sensor 4 max value
+    int maxvalue = 0;
+
     if (log.is_open()) {
+        string line;
         getline(log, line);
         while (getline(log, line)) {
-            string sensor1, sensor2, sensor3, sensor4, sensor5, sensor6;
+            string sensor4;
 
-            line = line.substr(line.find(",") + 1, line.size());
-            sensor1 = line.substr(0, line.find(","));
-            line = line.substr(line.find(",") + 1, line.size());
-            sensor2 = line.substr(0, line.find(","));
-            line = line.substr(line.find(",") + 1, line.size());
-            sensor3 = line.substr(0, line.find(","));
-            line = line.substr(line.find(",") + 1, line.size());
+            //Remove first 4 columns
+            int i = 0;
+            while (i < 4) {
+                line = line.substr(line.find(",") + 1, line.size());
+                i++;
+            }
+
+            //Extract data from 5th column
             sensor4 = line.substr(0, line.find(","));
-            line = line.substr(line.find(",") + 1, line.size());
-            sensor5 = line.substr(0, line.find(","));
-            line = line.substr(line.find(",") + 1, line.size());
-            sensor6 = line.substr(0, line.find(","));
 
-            if (sensor4max < atoi(sensor4.c_str())) {
-                sensor4max = atoi(sensor4.c_str());
+            //Set max value
+            if (maxvalue < atoi(sensor4.c_str())) {
+                maxvalue = atoi(sensor4.c_str());
             }
         }
 
-        cout << sensor4max;
+        //Save max value in file sensor4.txt
+        ofstream maxvalue_file;
+        maxvalue_file.open("/var/www/sensor4.txt");
+        maxvalue_file << maxvalue;
+
+        //Close files
+        maxvalue_file.close();
+        log.close();
 
     } else {
         printf("ERROR: File has not been opened.");
